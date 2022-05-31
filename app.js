d31573343755281a8acd1a3e26fe4b56f1c92a91
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2022-06-01 02:02:43 +0500

    refactor: change names according to rule 3.2 and 3.3 in index.html

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.className="task-list_item";
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
@@ -33,15 +34,14 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
-    label.id="label"
+    label.className='task label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     checkBox.className="checkbox"
     editInput.type="text";
-    editInput.className="task";
-    editInput.id="task-input";
+    editInput.className="task task-list_input";
+;
 
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
